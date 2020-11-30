4cff93aa39b6249413f04acce20c98042217f52c
Jamiu Makinde <successmakinde@gmail.com>
2020-11-30 11:07:46 +0100

    car details

--- car.dart
+++ car.dart
@@ -1,11 +1,21 @@
 class Car {
   // properties
-
+  var model, color, id, year, status;
   // Constructors
-  Car() {
-
+  Car(String Cmodel, String Ccolor, int Cid, int Cyear, String Cstatus) {
+    this.model = Cmodel;
+    this.color = Ccolor;
+    this.id = Cid;
+    this.year = Cyear;
+    this.status = Cstatus;
   }
 
   // methods
-  
+  showinfo() {
+    print('The Model of the car is :  ${model}');
+    print('The Color of the car is :  ${color}');
+    print('The Id of the car is :  ${id}');
+    print('The Year of the car is :  ${year}');
+    print('The Status of the car is :  ${status}');
+  }
 }
--- main.dart
+++ main.dart
@@ -1,5 +1,9 @@
 import 'car.dart';
 
 main() {
-  
+  var fleet = new Car("Toyota", "blue", 001, 2020, "available");
+  var fleet2 = new Car("Honda", "Red", 006, 2018, "leased");
+
+  fleet.showinfo();
+  fleet2.showinfo();
 }
